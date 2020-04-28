3fdef0960ad650e7e0cb43d37ae46124d2471fc1
Santiagobocel <santiagobocel10@gmail.com>
2020-04-28 12:49:00 -0600

    Nodo Bebida

---
+++ LabReposicion/ArbolB(estrella)/Bebida.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabReposicion.ArbolB_estrella_
+{
+    public class Bebida
+    {
+    }
+}
---
+++ LabReposicion/ArbolB(estrella)/Controller/BebidasController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabReposicion.ArbolB_estrella_.Controller
+{
+    public class BebidasController : ControllerBase
+    {
+        [HttpPost]
+        [Route("api/insertar")]
+        public ActionResult Insertar([FromBody] Bebida Soda)
+        {
+            if (ModelState.IsValid)
+            {
+                Estructura.ArbolB_Estrella_.Instance.Add(Soda.Nombre, Soda.Sabor, Soda.Volumen, Soda.Precio, Soda.Casa_Productora);
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
